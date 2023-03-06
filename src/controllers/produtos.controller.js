efe06d5c3c17f7c2833c2936928f88c9f9c6fe30
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-03-06 11:53:10 -0300

    Rota Produtos
    Post da rota de produtos.

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -2,19 +2,60 @@ const pool = require('../database/index');
 
 const produtosController = {
     pegaTodosProdutos: async (req, res) => {
+        const queryPegaTodosProdutos = 'SELECT * FROM produtos';
         try {
-            const queryPegaTodosProdutos = 'SELECT * FROM produtos';
             const [response] = await pool.query(queryPegaTodosProdutos);
-            res.status(200).json(response);
+            return res.status(200).json(response);
         } catch (error) {
+            res.status(500).json({message: 'Erro do servidor'});
             console.log('Erro ao pegar todos os produtos: ' + error);
         }
     },
-    insereProduto: async (req, res) => {
+    pegaProdutoCategoria: async (req, res) => {
+        const { categoria } = req.params;
+        const queryPegaProdutoCategoria = 'SELECT * FROM produtos WHERE categoria= ?';
         try {
-            const {nome, preco, categoria, descricao} = req.body;
-            console.log(req.body);
-            const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
+            const [response] = await pool.query(queryPegaProdutoCategoria, [categoria]);
+            return res.status(200).json(response);
+        } catch (error) {
+            res.status(500).json({message: 'Erro do servidor'});
+            console.log('Erro ao pegar produtos dessa categoria: ' + error);
+        }
+    },
+    insereProduto: async (req, res) => {
+        let {nome, preco, categoria, descricao} = req.body;
+
+        // Verificando o tamanho dos valores
+        if (nome.length > 50 || categoria.length > 30) {
+            return res.status(400).json({message: 'Número de caracteres do nome ou da categoria muito grandes.'});
+        }
+
+        // Verificando o tipo de dado do preço
+        if (typeof preco !== 'number') {
+            preco = parseFloat(preco);
+            console.log(typeof preco);
+            if (typeof preco === 'NaN') {
+                return res.status(400).json({message: 'O campo preço deve ser do tipo número.'});
+            }
+        }
+
+        // Verificando o tipo de dado dos campos
+        if ((typeof nome !== 'string') || (typeof categoria !== 'string') || (typeof descricao !== 'string')) {
+            return res.status(400).json({message: 'As colunas nome, categoria e descrição devem ser do tipo strings.'});
+        }
+
+        // Removendo espaços em branco
+        nome = nome.trim();
+        categoria = categoria.trim();
+        descricao = descricao.trim();
+
+        // Deixando as strings todas minúsculas
+        nome = nome.toLowerCase();
+        categoria = categoria.toLowerCase();
+        descricao = descricao.toLowerCase();
+
+        const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
+        try {
             const response = await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
             res.status(201).json(response);
         } catch (error) {
--- src/routes/produtos.router.js
+++ src/routes/produtos.router.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const produtosController = require('../controllers/produtos.controller');
 
-router.get("/", produtosController.pegaTodosProdutos)
-router.post("/", produtosController.insereProduto);
+router.get('/', produtosController.pegaTodosProdutos)
+router.get('/:categoria', produtosController.pegaProdutoCategoria);
+router.post('/', produtosController.insereProduto);
 
 module.exports = router;
