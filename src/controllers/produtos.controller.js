fe7579c1e56de34dd924b6965cb402cb96cd0ce1
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-03-06 09:56:48 -0300

    Rotas de Produtos Atualizadas

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -1,15 +1,27 @@
 const pool = require('../database/index');
 
 const produtosController = {
-   insereProduto: async (req, res) => {
-    try {
-        const {nome, preco, categoria, descricao} = req.body;
-        const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
-        const response = await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
-        res.status(201).json(response);
-    } catch (error) {
-        console.log('Erro ao registrar um novo produto: ' + error);
-    }
+    pegaTodosProdutos: async (req, res) => {
+        try {
+            const queryPegaTodosProdutos = 'SELECT * FROM produtos';
+            const response = await pool.query(queryPegaTodosProdutos);
+            res.status(200).json(response);
+        } catch (error) {
+            console.log('Erro ao pegar todos os produtos: ' + error);
+        }
+    },
+    insereProduto: async (req, res) => {
+        try {
+            res.status(200).send('olá mundo');
+
+            // const {nome, preco, categoria, descricao} = req.body;
+            // console.log(req.body)
+            // const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
+            // const response = await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
+            // res.status(201).json(response);
+        } catch (error) {
+            console.log('Erro ao registrar um novo produto: ' + error);
+        }
    }
 }
 
--- src/routes/produtos.router.js
+++ src/routes/produtos.router.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const produtosController = require('../controllers/produtos.controller');
 
+router.get("/", produtosController.pegaTodosProdutos)
 router.post("/", produtosController.insereProduto);
 
 module.exports = router;
