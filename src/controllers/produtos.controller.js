9c5bffaf375fbb5a2234abf345a62d2b1936c85a
Henrique de Moraes Botelho da Silva <henriquebotelho.imp@gmail.com>
2023-03-06 19:35:51 -0300

    Rota produtos
    Criar e atualizar produtos

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -12,7 +12,8 @@ const produtosController = {
         }
     },
     pegaProdutoCategoria: async (req, res) => {
-        const { categoria } = req.params;
+        let { categoria } = req.params;
+        categoria = categoria.toLowerCase();
         const queryPegaProdutoCategoria = 'SELECT * FROM produtos WHERE categoria= ?';
         try {
             const [response] = await pool.query(queryPegaProdutoCategoria, [categoria]);
@@ -27,28 +28,23 @@ const produtosController = {
 
         // Verificando o tamanho dos valores
         if (nome.length > 50 || categoria.length > 30) {
-            return res.status(400).json({message: 'Número de caracteres do nome ou da categoria muito grandes.'});
+            return res.status(400).json({message: 'Número de caracteres do nome ou da categoria muito grande.'});
         }
-
         // Verificando o tipo de dado do preço
         if (typeof preco !== 'number') {
             preco = parseFloat(preco);
-            console.log(typeof preco);
-            if (typeof preco === 'NaN') {
+            if (Number.isNaN(preco)) {
                 return res.status(400).json({message: 'O campo preço deve ser do tipo número.'});
             }
         }
-
-        // Verificando o tipo de dado dos campos
+        // Verificando o tipo de dado dos outros campos
         if ((typeof nome !== 'string') || (typeof categoria !== 'string') || (typeof descricao !== 'string')) {
             return res.status(400).json({message: 'As colunas nome, categoria e descrição devem ser do tipo strings.'});
         }
-
         // Removendo espaços em branco
         nome = nome.trim();
         categoria = categoria.trim();
         descricao = descricao.trim();
-
         // Deixando as strings todas minúsculas
         nome = nome.toLowerCase();
         categoria = categoria.toLowerCase();
@@ -57,10 +53,50 @@ const produtosController = {
         const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
         try {
             const response = await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
-            res.status(201).json(response);
+            res.status(201).json({response, criado: {nome,preco,categoria,descricao}});
         } catch (error) {
             console.log('Erro ao registrar um novo produto: ' + error);
         }
+   },
+   atualizaProduto: async (req, res) => {
+        const { id } = req.params;
+        let { nome, preco, categoria, descricao } = req.body;
+        
+        // Verificando o tamanho dos valores
+        if (nome.length > 50 || categoria.length > 30) {
+            return res.status(400).json({message: 'Número de caracteres do nome ou da categoria muito grande.'});
+        }
+        // Verificando o tipo de dado do preço
+        if (typeof preco !== 'number') {
+            preco = parseFloat(preco);
+            if (Number.isNaN(preco)) {
+                return res.status(400).json({message: 'O campo preço deve ser do tipo número.'});
+            }
+        }
+        // Verificando o tipo de dado dos outros campos
+        if ((typeof nome !== 'string') || (typeof categoria !== 'string') || (typeof descricao !== 'string')) {
+            return res.status(400).json({message: 'As colunas nome, categoria e descrição devem ser do tipo strings.'});
+        }
+        // Removendo espaços em branco
+        nome = nome.trim();
+        categoria = categoria.trim();
+        descricao = descricao.trim();
+        // Deixando as strings todas minúsculas
+        nome = nome.toLowerCase();
+        categoria = categoria.toLowerCase();
+        descricao = descricao.toLowerCase();
+        
+        const queryAtualizaProduto = 'UPDATE produtos SET nome = ?, preco= ?, categoria= ?, descricao= ? WHERE id= ?';
+        try {
+            const [response] = await pool.query(queryAtualizaProduto, [nome,preco,categoria,descricao,id]);
+            res.status(200).json({response, atualizado: {nome,preco,categoria,descricao}});
+        } catch (error) {
+            res.status(500).json({message: 'Erro do servidor.'});
+            console.log('Erro ao atualizar os dados: ' + error);
+        }
+   },
+   deletaProduto: async (req, res) => {
+    
    }
 }
 
--- src/routes/produtos.router.js
+++ src/routes/produtos.router.js
@@ -5,5 +5,7 @@ const produtosController = require('../controllers/produtos.controller');
 router.get('/', produtosController.pegaTodosProdutos)
 router.get('/:categoria', produtosController.pegaProdutoCategoria);
 router.post('/', produtosController.insereProduto);
+router.put('/:id', produtosController.atualizaProduto);
+router.delete('/:id', produtosController.deletaProduto);
 
 module.exports = router;
