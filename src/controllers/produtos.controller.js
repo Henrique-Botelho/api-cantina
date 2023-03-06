c33acaf85a93eadc43a2b3361e9e4aaab2c3be73
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-03-06 11:06:29 -0300

    Update produtos.controller.js

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -4,7 +4,7 @@ const produtosController = {
     pegaTodosProdutos: async (req, res) => {
         try {
             const queryPegaTodosProdutos = 'SELECT * FROM produtos';
-            const response = await pool.query(queryPegaTodosProdutos);
+            const [response] = await pool.query(queryPegaTodosProdutos);
             res.status(200).json(response);
         } catch (error) {
             console.log('Erro ao pegar todos os produtos: ' + error);
@@ -12,13 +12,11 @@ const produtosController = {
     },
     insereProduto: async (req, res) => {
         try {
-            res.status(200).send('olá mundo');
-
-            // const {nome, preco, categoria, descricao} = req.body;
-            // console.log(req.body)
-            // const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
-            // const response = await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
-            // res.status(201).json(response);
+            const {nome, preco, categoria, descricao} = req.body;
+            console.log(req.body);
+            const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
+            const response = await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
+            res.status(201).json(response);
         } catch (error) {
             console.log('Erro ao registrar um novo produto: ' + error);
         }
