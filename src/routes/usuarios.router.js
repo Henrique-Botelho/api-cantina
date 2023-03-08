9f2e381bf7b2cee4819c972fb74248e4fc970fcd
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-03-08 10:28:29 -0300

    Update usuarios.router.js

--- src/routes/usuarios.router.js
+++ src/routes/usuarios.router.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const usuariosController = require('../controllers/usuarios.controller');
 
-
+// Rota para cadastrar um novo usuário
 router.post('/cadastro', usuariosController.cadastraUsuario);
-router.post('/', usuariosController.cadastraUsuario);
+
+// Rota para logar um usuário
+router.post('/', usuariosController.login);
 
 module.exports = router;
