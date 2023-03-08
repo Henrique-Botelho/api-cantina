84abe8da43c7ff6ef19503537129e2972cb48bda
Abner Mendes <93213670+ClaushSouza@users.noreply.github.com>
2023-03-08 10:48:43 -0300

    Rota compras
    Crie a rota de criar as compras

--- src/controllers/compras.controller.js
+++ src/controllers/compras.controller.js
@@ -1,7 +1,17 @@
-const pool = require('../database/index');
+const pool = require("../database/index");
 
 const comprasController = {
-
-}
+  criarCompra: async (req, res) => {
+    const { id_cliente, compra, total, dataHora } = req.body;
+    try {
+        const queryInsereCompra = "INSERT INTO compras (id_cliente, compra, total, data_hora) VALUES (?, ?, ?, ?)"
+      const resultado = await pool.query(queryInsereCompra, [id_cliente, compra, total, dataHora]);
+      res.status(200).json({message:"Compra criada com sucesso!"});
+    } catch (erro) {
+      console.error(erro);
+      res.status(500).send("Erro ao criar a compra");
+    }
+  },
+};
 
 module.exports = comprasController;
--- src/routes/compras.router.js
+++ src/routes/compras.router.js
@@ -2,7 +2,15 @@ const express = require('express');
 const router = express.Router();
 const comprasController = require('../controllers/compras.controller');
 
-router.post('/');
+// Rota de criar nova compra
+router.post('/', comprasController.criarCompra);
 
+// Rota de alterar a compra
+
+// Rota de excluir a compra 
+
+// Rota de pegar todas as compras 
+
+// Rota pegar todas as compras de um usuario especifico
 
 module.exports = router;
