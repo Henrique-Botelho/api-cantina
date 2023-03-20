61e2ce6488bd1d29f8d8e3f02075aa1d5324ce6e
Guigite <guicunhaalves@gmail.com>
2023-03-20 11:51:45 -0300

    validando compras.controller.js (incompleto). Dia 20/03

--- src/controllers/compras.controller.js
+++ src/controllers/compras.controller.js
@@ -6,43 +6,43 @@ const comprasController = {
     const queryListarCompras = 'SELECT * FROM compras';
     try {
       const [resultado] = await pool.query(queryListarCompras);
-      res.status(200).json({ statusCode: 200, resultado });
+      res.status(200).json({ status: 200, resultado });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ errorCode: 500, message: 'Erro do servidor' });
+      console.log("Erro ao listar compras." + error);
+      return res.status(500).json({ status: 500, message: 'Erro no contato com o servidor.' });
     }
   },
   // Função de pegar todas as compras de um cliente específico
   listarComprasPorUsuario: async (req, res) => {
     const { numero } = req.params;
-    const queryVerificaCliente = 'SELECT id FROM clientes WHERE numero=?';
+    const queryVerificaCliente = 'SELECT id FROM clientes WHERE numero = ?';
     const queryListarComprasPorUsuario = 'SELECT * FROM compras WHERE id_cliente = ?';
     try {
       // Encontrando o cliente pelo número
-      const [responseUsuario] = await pool.query(queryVerificaCliente, [numero]);
+      const [responseUsuario] = await pool.query(queryVerificaCliente, [numero])
       if (responseUsuario.length === 0) {
-        return res.status(401).json({ errorCode: 401, message: 'Usuário não encontrado.' });
+        return res.status(404).json({ status: 404, message: 'Usuário não encontrado.' })
       }
-      const [response] = await pool.query(queryListarComprasPorUsuario, [responseUsuario[0].id]);
-      return res.status(200).json({ statusCode: 200, response });
+      const [response] = await pool.query(queryListarComprasPorUsuario, [responseUsuario[0].id])
+      return res.status(200).json({ status: 200, response });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ errorCode: 500, message: 'Erro do servidor.' });
+      console.log("Erro ao listar compras do usuário." + error);
+      return res.status(500).json({ status: 500, message: 'Erro no contato com o servidor.' })
     }
   },
   // Função para criar compra
   criarCompra: async (req, res) => {
     const { id_cliente, compra, total, dataHora } = req.body;
     if (!id_cliente || !compra || !total || !dataHora) {
-      return res.status(400).json({ errorCode: 400, message: 'Faltam dados' });
+      return res.status(400).json({ status: 400, message: 'Faltam dados para completar o registro.' });
     }
     const queryInsereCompra = 'INSERT INTO compras (id_cliente, compra, total, dataHora) VALUES (?, ?, ?, ?)';
     try {
-      const resultado = await pool.query(queryInsereCompra, [id_cliente, compra, total, dataHora]);
-      res.status(200).json({ statusCode: 200, message: 'Compra criada com sucesso!' });
+      const response = await pool.query(queryInsereCompra, [id_cliente, compra, total, dataHora]);
+      res.status(200).json({ status: 200, message: 'Compra criada com sucesso!' });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ errorCode: 500, message: 'Erro do servidor' });
+      console.log(error);
+      return res.status(500).json({ status: 500, message: 'Erro no contato com o servidor.' });
     }
   },
   // Função para alterar compra que foi criada
@@ -50,18 +50,18 @@ const comprasController = {
     const { id } = req.params;
     const { compra, total, dataHora } = req.body;
     if (!compra || !total || !dataHora) {
-      return res.status(400).json({ errorCode: 400, message: 'Faltam dados' });
+      return res.status(400).json({ status: 400, message: 'Faltam dados' });
     }
     const queryalterarCompra = 'UPDATE compras SET compra=?, total=?, dataHora=? WHERE id=?';
     try {
       const resultado = await pool.query(queryalterarCompra, [compra, total, dataHora, id]);
       if (resultado.affectedRows === 0) {
-        return res.status(404).json({ errorCode: 404, message: 'Compra não encontrada' });
+        return res.status(404).json({ status: 404, message: 'Compra não encontrada' });
       }
-      return res.status(200).json({ statusCode: 200, message: 'Compra atualizada com sucesso!' });
+      return res.status(200).json({ status: 200, message: 'Compra atualizada com sucesso!' });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ errorCode: 500, message: 'Erro do servidor' });
+      console.log(error);
+      return res.status(500).json({ status: 500, message: 'Erro no contato com o servidor.' });
     }
   },
   // Excluir a compra pelo ID
@@ -71,12 +71,12 @@ const comprasController = {
     try {
       const resultado = await pool.query(queryExcluirCompra, [id]);
       if (resultado.affectedRows === 0) {
-        return res.status(404).json({ errorCode: 404, message: 'Compra não encontrada' });
+        return res.status(404).json({ status: 404, message: 'Compra não encontrada' });
       }
-      return res.status(200).json({ statusCode: 200, message: 'Compra excluída com sucesso!' });
+      return res.status(200).json({ status: 200, message: 'Compra excluída com sucesso!' });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ errorCode: 500, message: 'Erro do servidor' });
+      console.log(error);
+      return res.status(500).json({ status: 500, message: 'Erro no contato com o servidor.' });
     }
   },
   // Excluir todas as compras de um cliente pelo ID
@@ -88,16 +88,16 @@ const comprasController = {
     try {
       const [response] = await pool.query(queryVerificaCliente, [id_cliente]);
       if (response.length === 0) {
-        return res.status(401).json({errorCode: 401, message: 'Esse cliente não existe.'});
+        return res.status(401).json({status: 401, message: 'Esse cliente não existe.'});
       }
       const resultado = await pool.query(queryExcluirCompras, [id_cliente]);
       if (resultado.affectedRows === 0) {
-        return res.status(404).json({ errorCode: 404, message: 'Nenhuma compra encontrada para este cliente' });
+        return res.status(404).json({ status: 404, message: 'Nenhuma compra encontrada para este cliente' });
       }
-      return res.status(200).json({ statusCode: 200, message: 'Compras excluídas com sucesso!' });
+      return res.status(200).json({ status: 200, message: 'Compras excluídas com sucesso!' });
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ errorCode: 500, message: 'Erro do servidor' });
+      console.log(error);
+      return res.status(500).json({ status: 500, message: 'Erro no contato com o servidor.' });
     }
   },
 };
