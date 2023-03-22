733736f03009067061cbc3adea17887383afff00
heitorsclaudino <heitorsclaudino04@gmail.com>
2023-03-22 09:26:37 -0300

    Update produtos.controller.js
    validações produtos feitas

--- src/controllers/produtos.controller.js
+++ src/controllers/produtos.controller.js
@@ -1,30 +1,33 @@
 const pool = require('../database/index');
 
 const produtosController = {
-    /**
-        * Função para pegar todos os produtos do banco de dados.
-        */
     pegaTodosProdutos: async (req, res) => {
-
         const queryPegaTodosProdutos = 'SELECT * FROM produtos';
+
         try {
             const [response] = await pool.query(queryPegaTodosProdutos);
             return res.status(200).json(response);
         } catch (error) {
-            console.log('Erro ao pegar todos os produtos: ' + error);
-            return res.status(500).json({errorCode: 500, message: 'Erro do servidor'});
+            console.log('Erro ao listar todos os produtos: ' + error);
+            return res.status(500).json({status: 500, message: 'Erro no contato com o servidor.'});
         }
     },
     pegaProdutoCategoria: async (req, res) => {
         let { categoria } = req.params;
+
+        //Validações
+        if(!categoria){
+            return res.status(400).json({status: 400, message: 'Envie uma categoria para realizar a operação.'});
+        }
         categoria = categoria.toLowerCase();
+
         const queryPegaProdutoCategoria = 'SELECT * FROM produtos WHERE categoria= ?';
         try {
             const [response] = await pool.query(queryPegaProdutoCategoria, [categoria]);
             return res.status(200).json(response);
         } catch (error) {
-            console.log('Erro ao pegar produtos dessa categoria: ' + error);
-            return res.status(500).json({errorCode: 500, message: 'Erro do servidor'});
+            console.log('Erro ao listar produtos dessa categoria: ' + error);
+            return res.status(500).json({status: 500, message: 'Erro no contato com o servidor.'});
         }
     },
     insereProduto: async (req, res) => {
@@ -32,35 +35,35 @@ const produtosController = {
 
         // Validações
         if (!nome || !preco || !categoria || !descricao) {
-            return res.status(400).json({errorCode: 400, message: 'Todos os campos devem ser enviados.'})
+            return res.status(400).json({status: 400, message: 'Todos os campos devem ser enviados.'});
         }
 
         if (nome.length > 50 || categoria.length > 30) {
-            return res.status(400).json({errorCode: 400, message: 'Número de caracteres do nome ou da categoria muito grande.'});
+            return res.status(400).json({status: 400, message: 'Número de caracteres do nome ou da categoria muito grande.'});
         }
         if (typeof preco !== 'number') {
             preco = parseFloat(preco);
-            if (Number.isNaN(preco)) {
-                return res.status(400).json({errorCode: 400, message: 'O campo preço deve ser do tipo número.'});
+            if (Number.isNaN(preco)) { // is not number
+                return res.status(400).json({status: 400, message: 'O campo preço deve ser do tipo número.'});
             }
         }
         if ((typeof nome !== 'string') || (typeof categoria !== 'string') || (typeof descricao !== 'string')) {
-            return res.status(400).json({errorCode: 400, message: 'As colunas nome, categoria e descrição devem ser do tipo strings.'});
+            return res.status(400).json({status: 400, message: 'As colunas nome, categoria e descrição devem ser do tipo strings.'});
         }
-        nome = nome.trim();
+        nome = nome.trim(); //tirando os espaços do começo e fim da string
         categoria = categoria.trim();
         descricao = descricao.trim();
-        nome = nome.toLowerCase();
+        nome = nome.toLowerCase(); //deixando a string em letras minúsculas
         categoria = categoria.toLowerCase();
         descricao = descricao.toLowerCase();
 
         const queryInsereProduto = 'INSERT INTO produtos (nome, preco, categoria, descricao) VALUES (?, ?, ?, ?)';
         try {
-            const response = await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
-            return res.status(201).json({message: 'Produto criado com sucesso!', criado: {nome,preco,categoria,descricao}});
+            await pool.query(queryInsereProduto, [nome, preco, categoria, descricao]);
+            return res.status(201).json({message: 'Produto criado com sucesso!'});
         } catch (error) {
             console.log('Erro ao registrar um novo produto: ' + error);
-            return res.status(500).json({errorCode: 500, message: 'Erro do servidor.'})
+            return res.status(500).json({status: 500, message: 'Erro no contato com o servidor.'})
         }
    },
    atualizaProduto: async (req, res) => {
@@ -69,22 +72,22 @@ const produtosController = {
     
         // Validação
         if (!nome || !preco || !categoria || !descricao) {
-            return res.status(400).json({errorCode: 400, message: 'Todos os campos devem ser enviados.'})
+            return res.status(400).json({status: 400, message: 'Todos os campos devem ser enviados.'})
         }
         if (!id) {
-            return res.status(400).json({errorCode: 400, message: 'É necessário informar o id do produto que deseja alterar.'});
+            return res.status(400).json({status: 400, message: 'É necessário informar o id do produto que deseja alterar.'});
         }
         if (nome.length > 50 || categoria.length > 30) {
-            return res.status(400).json({errorCode: 400, message: 'Número de caracteres do nome ou da categoria muito grande.'});
+            return res.status(400).json({status: 400, message: 'Número de caracteres do nome ou da categoria muito grande.'});
         }
         if (typeof preco !== 'number') {
             preco = parseFloat(preco);
             if (Number.isNaN(preco)) {
-                return res.status(400).json({errorCode: 400, message: 'O campo preço deve ser do tipo número.'});
+                return res.status(400).json({status: 400, message: 'O campo preço deve ser do tipo número.'});
             }
         }
         if ((typeof nome !== 'string') || (typeof categoria !== 'string') || (typeof descricao !== 'string')) {
-            return res.status(400).json({errorCode: 400, message: 'As colunas nome, categoria e descrição devem ser do tipo strings.'});
+            return res.status(400).json({status: 400, message: 'As colunas nome, categoria e descrição devem ser do tipo strings.'});
         }
         nome = nome.trim();
         categoria = categoria.trim();
@@ -95,11 +98,11 @@ const produtosController = {
         
         const queryAtualizaProduto = 'UPDATE produtos SET nome = ?, preco= ?, categoria= ?, descricao= ? WHERE id= ?';
         try {
-            const [response] = await pool.query(queryAtualizaProduto, [nome,preco,categoria,descricao,id]);
-            return res.status(200).json({message: 'Produto atualizado com sucesso!', atualizado: {nome,preco,categoria,descricao}});
+            await pool.query(queryAtualizaProduto, [nome,preco,categoria,descricao,id]);
+            return res.status(200).json({message: 'Produto atualizado com sucesso!'});
         } catch (error) {
             console.log('Erro ao atualizar os dados: ' + error);
-            return res.status(500).json({errorCode: 500, message: 'Erro do servidor.'});
+            return res.status(500).json({status: 500, message: 'Erro no contato com o servidor.'});
         }
    },
    deletaProduto: async (req, res) => {
@@ -107,15 +110,15 @@ const produtosController = {
 
         // Validação
         if (!id) {
-            return res.status(400).json({errorCode: 400, message: 'É necessário informar o id do produto que deseja alterar.'});
+            return res.status(400).json({status: 400, message: 'É necessário informar o id do produto que deseja alterar.'});
         }
         const queryDeletaProduto = 'DELETE FROM produtos WHERE id= ?';
         try {
-            const [response] = await pool.query(queryDeletaProduto, [id]);
+            await pool.query(queryDeletaProduto, [id]);
             return res.status(200).json({message: 'Produto excluido com sucesso!'});
         } catch(error) {
             console.log('Erro ao tentar deletar esse produto: ' + error);
-            return res.status(500).json({errorCode: 500, message: 'Erro no servidor.'});
+            return res.status(500).json({status: 500, message: 'Erro no contato com o servidor.'});
         }
    }
 }
