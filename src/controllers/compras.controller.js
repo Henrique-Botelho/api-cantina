d5503ee7941c3e4949df56f44cd0a169dd943cee
Abner Mendes <93213670+ClaushSouza@users.noreply.github.com>
2023-04-14 17:04:30 -0300

    Update compras.controller.js

--- src/controllers/compras.controller.js
+++ src/controllers/compras.controller.js
@@ -115,34 +115,42 @@ const comprasController = {
     const { numero } = req.params;
 
     const queryVerificaCliente = 'SELECT id FROM clientes WHERE numero=?';
+    const queryVerificaCompras = 'SELECT * FROM compras WHERE id_cliente=?';
     const queryExcluirCompras = 'DELETE FROM compras WHERE id_cliente=?';
     try {
       const [clientes] = await pool.query(queryVerificaCliente, [numero]);
       if (clientes.length === 0) {
         return res.status(401).json({ status: 401, message: 'Este cliente não existe.' });
       }
+      
+      let compras = await Promise.all(
+        clientes.map(cliente => pool.query(queryVerificaCompras, [cliente.id]))
+      );
+      
+      compras = compras.flat();
+
+      if (compras.length === 0) {
+        return res.status(404).json({ status: 404, message: 'Nenhuma compra encontrada para este cliente.' });
+      }
+
       let algumaCompraExcluida = false;
-      for (const cliente of clientes) {
-        const resultado = await pool.query(queryExcluirCompras, [cliente.id]);
+      for (const compra of compras) {
+        const resultado = await pool.query(queryExcluirCompras, [compra.id_cliente]);
         if (resultado.affectedRows !== 0) {
           algumaCompraExcluida = true;
         }
       }
+
       if (algumaCompraExcluida) {
         return res.status(200).json({ status: 200, message: 'Compras excluídas com sucesso!' });
       } else {
-        // Verifica se nenhuma compra foi excluída
-        const [compras] = await pool.query('SELECT * FROM compras WHERE id_cliente IN (SELECT id FROM clientes WHERE numero=?)', [numero]);
-        if (compras.length === 0) {
-          return res.status(404).json({ status: 404, message: 'Nenhuma compra encontrada para este cliente.' });
-        }
-        return res.status(200).json({ status: 200, message: 'Compras excluídas com sucesso!' });
-      }      
+        return res.status(404).json({ status: 404, message: 'Nenhuma compra encontrada para este cliente.' });
+      }
     } catch (error) {
       console.log("Erro ao Deletar todas as compras" + error);
       return res.status(500).json({ status: 500, message: 'Erro no contato com o servidor.' });
     }
-  },
+  }
 };
 
 module.exports = comprasController;
