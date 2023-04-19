0bc0dc5d868181fe1a2cc89208e74fe67afcbe81
Henrique de Moraes Botelho da Silva <98782303+Henrique-Botelho@users.noreply.github.com>
2023-04-19 08:49:36 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,9 +23,6 @@ app.use('/api/compras', comprasRouter); //api/produtos. Ela usa o método use() 
 
 
 //Ele usa o método listen() do objeto app e uma função de retorno de chamada para imprimir uma mensagem no console.log, informando que o servidor está em execução.
-app.listen({
-  host: '0.0.0.0',
-  port: 3000
-}, () => {
-    console.log(`Servidor rodando na porta ${3000}...`);
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}...`);
 });
