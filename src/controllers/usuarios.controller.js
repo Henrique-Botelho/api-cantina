f3d3b5f897a86f4575350a5a0ba4ca09e36d5243
Abner Mendes <abnermendesdesouza@gmail.com>
2023-04-26 14:51:21 -0300

    Update usuarios.controller.js

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -6,11 +6,11 @@ const { SECRET } = require('../config/config');
 // Criando objeto "usuariosController"
 const usuariosController = {
     cadastraUsuario: async (req, res) => {
-        // Recebendo as variáveis "userName", "senha", "confirmaSenha" do body.
-        let { userName, senha, confirmaSenha } = req.body;
+        // Recebendo as variáveis "userName", "email", "senha", "confirmaSenha" do body.
+        let { userName, email, senha, confirmaSenha } = req.body;
 
         // Verificando se todos os campos estão preenchidos
-        if (!userName || !senha || !confirmaSenha) {
+        if (!userName || !email || !senha || !confirmaSenha) {
             return res.status(400).json({errorCode: 400, message: 'Faltam dados.'});
         }
         // Verificando a igualdade nos campos "senha" e "confirmaSenha".
@@ -18,24 +18,25 @@ const usuariosController = {
             return res.status(400).json({errorCode: 400, message: 'As senhas não são iguais.'});
         }
         // Verificando se todos os dados inseridos são do tipo string.
-        if (typeof userName !== 'string' || typeof senha !== 'string' || typeof confirmaSenha !== 'string') {
+        if (typeof userName !== 'string' || typeof email !== 'string' || typeof senha !== 'string' || typeof confirmaSenha !== 'string') {
             return res.status(400).json({errorCode: 400, message: "Todos os dados devem ser do tipo String."});
         }
         // Verificando se quantidade de caracteres inseridos está entre o mínimo e o máximo pedido.
-        if (senha.length < 8 || senha.length > 20 || userName.length > 50 || userName.length < 3) {
-            return res.status(400).json({errorCode: 400, message: 'Quantidade de caracteres errada. (userName): min. 3, max. 50; (senha): min. 8, max. 20.'});
+        if (senha.length < 8 || senha.length > 20 || userName.length > 50 || userName.length < 3 || email.length > 255) {
+            return res.status(400).json({errorCode: 400, message: 'Quantidade de caracteres errada. (userName): min. 3, max. 50; (email): max. 255; (senha): min. 8, max. 20.'});
         }
 
         try {
-            // Verificando se já existe algum usuário com o nome inserido.
-            const queryConfereUsuario = 'SELECT * FROM usuarios WHERE userName= ?';
-            const [responseVerifica] = await pool.query(queryConfereUsuario, [userName]);
+            // Verificando se já existe algum usuário com o nome ou email inserido.
+            const queryConfereUsuario = 'SELECT * FROM usuarios WHERE userName= ? OR email= ?';
+            const [responseVerifica] = await pool.query(queryConfereUsuario, [userName, email]);
             if (responseVerifica.length !== 0) {
-                return res.status(400).json({errorCode: 400, message: 'Esse usuário já existe.'});
+                return res.status(400).json({errorCode: 400, message: 'Esse usuário ou email já existe.'});
             }
 
             // Cadastrando novo usuário
             userName = userName.trim();
+            email = email.trim();
             // Cadastrando nova senha.
             senha = senha.trim();
 
@@ -45,8 +46,8 @@ const usuariosController = {
             const senhaHash = await bcrypt.hash(senha, salt);
 
             // Inserindo um novo cadastro na tabela usuários.
-            const queryCadastraUsuario = 'INSERT INTO usuarios (userName, senha) VALUES (?,?)';
-            const [response] = await pool.query(queryCadastraUsuario, [userName, senhaHash]);
+            const queryCadastraUsuario = 'INSERT INTO usuarios (userName, email, senha) VALUES (?,?,?)';
+            const [response] = await pool.query(queryCadastraUsuario, [userName, email, senhaHash]);
 
             // Resposta ao cliente que seu usuário foi cadastrado.
             res.status(201).json({message: 'Usuário cadastrado com sucesso!'});
@@ -57,51 +58,43 @@ const usuariosController = {
             return res.status(500).json({errorCode:500, message: 'Erro no servidor.'});
         }
     },
-    login: async (req, res) => {
-        const { userName, senha } = req.body;
-        // Recebendo as variáveis "userName" e "senha" do body.
 
-        // Verificando se todos os campos estão preenchidos
+    login: async (req, res) => {
+        // Recebendo as variáveis "userName" e "senha" do body.
+        const { userName, senha } = req.body;
+
+        // Verificando se todos os campos estão preenchidos.
         if (!userName || !senha) {
-            return res.status(400).json({errorCode: 400, message: 'Username ou senha estão vazios.'});
-        }
-        // Verificando se todos os dados inseridos são do tipo string.
-        if (typeof userName !== 'string' || typeof senha !== 'string') {
-            return res.status(400).json({errorCode: 400, message: 'Os dados devem ser do tipo string.'});
+            return res.status(400).json({ errorCode: 400, message: 'Faltam dados.' });
         }
 
         try {
-            // Verificando se o nome de usuário inserido existe.
-            const queryVerificaUsuario = 'SELECT * FROM usuarios WHERE userName=?';
+            // Verificando se o usuário existe no banco de dados.
+            const queryVerificaUsuario = 'SELECT * FROM usuarios WHERE userName = ?';
             const [response] = await pool.query(queryVerificaUsuario, [userName]);
-
             if (response.length === 0) {
-                return res.status(404).json({errorCode: 404, message: 'Usuário não encontrado.'});
-                // Respota ao usuário caso o nome inserido não exista.
-            }
-            
-            // Verificando se a senha inserida é a mesma registrada no Banco de Dados.
-            const verificaSenha = await bcrypt.compare(senha, response[0].senha);
-            if (!verificaSenha) {
-                return res.status(400).json({errorCode: 400, message: 'Senha incorreta.'});
+                return res.status(400).json({ errorCode: 400, message: 'Usuário não encontrado.' });
             }
 
-            // Gerando token de autorização na autenticação
-            const token = jwt.sign({
-                id: response[0].id,
-                userName: response[0].userName
-            }, SECRET, {
-                expiresIn: '1d'
-            });
-            return res.status(200).json({message: 'Autenticação realizada com sucesso!', token});
-            // Resposta ao cliente que a autenticação foi realizada.
+            // Comparando a senha fornecida com o hash armazenado no banco de dados.
+            const senhaCorreta = await bcrypt.compare(senha, response[0].senha);
+            if (!senhaCorreta) {
+                return res.status(401).json({ errorCode: 401, message: 'Senha incorreta.' });
+            }
 
-        } catch (err) {
-            console.log(err);
-            res.status(500).json({errorCode: 500, message: 'Erro no servidor'});
+            // Gerando o token de autenticação.
+            const token = jwt.sign({ id: response[0].id }, SECRET, { expiresIn: '1h' });
+
+            // Resposta ao cliente com o token de autenticação.
+            res.status(200).json({ token });
+
+        } catch (error) {
             // Tratamento de erros durante o "Try"
+            console.log(error);
+            return res.status(500).json({ errorCode: 500, message: 'Erro no servidor.' });
         }
     }
-}
+};
 
 module.exports = usuariosController;
+
