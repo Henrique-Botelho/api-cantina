aad3ba131fc3462c3553b7e4ef5c4611f5a327f3
Heitor da Silva Claudino <98860705+heitorsclaudino@users.noreply.github.com>
2023-05-10 09:09:42 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -4,11 +4,11 @@ const { DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT } = require('../config/confi
 
 //Ao criar um pool de conexão, o aplicativo pode gerenciar e reutilizar conexões de banco de dados existentes em vez de criar uma nova conexão para cada solicitação. Isso melhora o desempenho do aplicativo, reduz a sobrecarga do banco de dados e permite que o aplicativo execute várias consultas simultaneamente.
 const pool = mysql.createPool({
-    host: DB_HOST || localhost, //Essas informações incluem o nome do host do banco de dados (DB_HOST)
-    user: DB_USER || root, //o nome de usuário
-    password: DB_PASS || "", //a senha 
-    database: DB_NAME || cantina, // nome do banco de dados
-    port: DB_PORT || 3306 // a porta do servidor
+    host: DB_HOST, //Essas informações incluem o nome do host do banco de dados (DB_HOST)
+    user: DB_USER, //o nome de usuário
+    password: DB_PASS, //a senha 
+    database: DB_NAME, // nome do banco de dados
+    port: DB_PORT // a porta do servidor
 });
 
 module.exports = pool.promise();
