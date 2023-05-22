2abd68635c2f687473d6662a9e27db46bb9ca969
Henrique Botelho <henriquebotelho.imp@gmail.com>
2023-05-22 11:58:32 -0300

    Update usuarios.controller.js

--- src/controllers/usuarios.controller.js
+++ src/controllers/usuarios.controller.js
@@ -80,7 +80,7 @@ const usuariosController = {
                 return res.status(401).json({ errorCode: 401, message: 'Senha incorreta.' });
             }
     
-            const token = jwt.sign({ email: response[0].email }, SECRET, { expiresIn: '60s' });
+            const token = jwt.sign({ email: response[0].email }, SECRET, { expiresIn: '24h' });
     
             res.status(200).json({ token });
     
