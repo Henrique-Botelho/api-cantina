d1ce87d3ffbdcd27dd33f3435ff651d93a7e581b
Henrique-Botelho <henriquebotelho.imp@gmail.com>
2023-05-31 17:05:23 -0300

    Update clientes.controller.js

--- src/controllers/clientes.controller.js
+++ src/controllers/clientes.controller.js
@@ -44,8 +44,8 @@ const clientesController = {
             await pool.query(queryInsereCliente, [nome, numero, email]);
 
             return res.status(201).json({ message: 'Cliente cadastrado com sucesso!' });
-        } catch (error) {
-            console.log('Erro ao cadastrar cliente' + error);
+        } catch (e) {
+            console.log(e);
             return res.status(500).json({ message: 'Erro no contato com o servidor.' });
         }
     },
@@ -57,9 +57,9 @@ const clientesController = {
             // Fazendo a operação.
             const [response] = await pool.query(queryListaClientes);
             return res.status(200).json(response);
-        } catch (error) {
+        } catch (e) {
             // Resposta ao usuario que sua operação não foi realizada.
-            console.log('Erro ao listar todos os clientes: ' + error);
+            console.log(e);
             // Tratamento de erros durante o "Try"
             return res.status(500).json({ message: 'Erro no contato com o servidor.' });
         }
@@ -70,88 +70,62 @@ const clientesController = {
         let { nome, numero, email } = req.body;
 
         if (!id) {
-            return res.status(400).json({ message: 'É necessário informar o id do cliente a ser atualizado.' });
+            return res.status(400).json({ message: 'É necessário informar o id do cliente.' });
         }
 
-        const verificaID = 'SELECT * FROM clientes WHERE id = ?';
+        if (!nome || !numero || !email) {
+            return res.status(400).json({ message: "Todos os campos devem ser enviados!" });
+        }
+        
+        if (numero.length < 8 || nome.length > 50) {
+            return res.status(400).json({ message: 'Quantidade de caracteres para nome e/ou telefone inválida.' });
+        }
+
+        if (!validarNome.test(nome)) {
+            return res.status(400).json({ message: 'Nome deve conter apenas letras e espaços.' });
+        }
+
+        if (!validarNumero.test(numero)) {
+            return res.status(400).json({ message: 'Número deve conter apenas números.' });
+        }
+        
+        nome = nome.trim();
+        numero = numero.trim();
+        email = email.trim();
 
         try {
-            const [rows] = await pool.query(verificaID, [id]);
-
-            if (rows.length === 0) {
-                return res.status(404).json({ message: 'Cliente não encontrado. Não é possível atualizar.' });
-            }
-
-            if (nome) {
-                if (!validarNome.test(nome)) {
-                    return res.status(400).json({ message: 'Nome deve conter apenas letras e espaços.' });
-                }
-                if (nome.length < 8 || nome.length > 50) {
-                    return res.status(400).json({ message: 'Quantidade de caracteres para nome inválida.' });
-                }
-            } else {
-                return res.status(400).json({ message: "Você deve colocar um nome para esse cliente!" });
-            }
-            
-            if (numero) {
-                if (!validarNumero.test(numero)) {
-                    return res.status(400).json({ message: 'Número deve conter apenas números.' });
-                }
-                if (numero.length !== 11) {
-                    return res.status(400).json({ message: 'Quantidade de caracteres para número inválida.' });
-                }
-            } else {
-                return res.status(400).json({ message: "Você deve colocar um número para esse cliente!" });
-            }
-            
-            if (!email) {
-                return res.status(400).json({ message: "Você deve colocar um email para esse cliente!" });
-            }
-
-            nome = nome.trim();
-            numero = numero.trim();
-            email = email.trim();
-
             const queryAtualizaCliente = 'UPDATE clientes SET nome = ?, numero = ?, email = ? WHERE id = ?';
             await pool.query(queryAtualizaCliente, [nome, numero, email, id]);
 
             return res.status(200).json({ message: 'Cliente atualizado com sucesso!' });
-        } catch (error) {
-            console.log('Erro ao atualizar cliente: ' + error);
+        } catch (e) {
+            console.log(e);
             return res.status(500).json({ message: 'Erro no contato com o servidor.' });
         }
     },
 
     deletaCliente: async (req, res) => {
         const { id } = req.params;
-        const ID = parseInt(id);
 
-        if (!ID) {
-            return res.json({ message: 'É necessário informar o id do cliente a ser deletado.' }).status(400);
+        if (!id) {
+            return res.status(400).json({ message: 'É necessário informar o id do cliente.' });
         }
 
-        const queryBuscaCompras = "SELECT * FROM compras WHERE id_cliente=? AND status=0";
-
-        const queryBuscaCliente = 'SELECT * FROM clientes WHERE id = ?';
-
+        
         try {
-            const [comprasNaoPagas] = await pool.query(queryBuscaCompras, [ID]);
+            const queryBuscaCompras = "SELECT * FROM compras WHERE id_cliente=? AND status=0";
+            const [comprasNaoPagas] = await pool.query(queryBuscaCompras, [id]);
 
             if (comprasNaoPagas.length !== 0) {
                 return res.status(400).json({ message: "Esse cliente possui compras não pagas!" });
             }
 
-            const [rows] = await pool.query(queryBuscaCliente, ID);
-
-            if (rows.length === 0) {
-                return res.status(404).json({ message: 'Cliente não encontrado.' });
-            }
             const queryDeletaCliente = 'DELETE FROM clientes WHERE id = ?';
-            await pool.query(queryDeletaCliente, ID);
+            await pool.query(queryDeletaCliente, [id]);
             return res.status(200).json({ message: 'Cliente deletado com sucesso.' });
-        } catch (error) {
-            console.log('Erro ao tentar deletar o cliente: ' + error);
-            return res.status(400).json({ message: 'Ocorreu um erro inesperado!' }).status(500);
+        } catch (e) {
+            console.log(e);
+            return res.status(500).json({ message: 'Ocorreu um erro inesperado!' });
         }
     }
 };
